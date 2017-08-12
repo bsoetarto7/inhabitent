c42cf8518d54a326f7e7dc7f7e1d6a3e6b974e24
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-12 16:23:23 -0700

    Added styling for header/navigation menu

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -20,15 +20,18 @@
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header flex-container-no-wrap container-fluid" role="banner">
-				<div class="site-branding flex-item-70">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</div><!-- .site-branding -->
+			<header id="masthead" class="site-header" role="banner">
+				<div class="container-fluid flex-container-no-wrap">
+					<div class="site-branding flex-item-70">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<img src=<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-tent.svg alt="">
+					</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation flex-item-30" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+					<nav id="site-navigation" class="main-navigation flex-item-30" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
