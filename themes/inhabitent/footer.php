d858286e27e20ab353b577f38c0227c39a4b7376
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-12 16:06:49 -0700

    Added footer styling

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,13 +10,33 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<div class="site-footer-items container-fluid">
+					<div class="flex-container-no-wrap">
+						<div class="footer-items flex-item-25">
+							<h2 class="text-uppercase">contact info</h2>
+							<p class="contact-meta">
+								<i class="fa fa-envelope"></i>
+								<a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+							</p>
+							<p class="contact-meta">
+								<i class="fa fa-phone" aria-hidden="true"></i>
+								<a href="tel:7784567891">778-456-7891</a>
+							</p>
+						</div>
+						<div class="footer-items flex-item-25">
+							<h2 class="text-uppercase">business hours</h2>
+							<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
+							<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
+							<p><span class="day-of-week">Sunday:</span> Closed</p>
+						</div>
+						<div class="footer-items flex-item-50">
+							<img src=<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-text.svg alt="Company logo"/>
+						</div>
+					</div>
+					<p class="text-center text-uppercase footer-copyright">copyright © <?php echo date('Y') ?> inhabitent</p>
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
-
 		<?php wp_footer(); ?>
-
 	</body>
 </html>
