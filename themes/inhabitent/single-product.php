c434b067ba8af89a09e1f2dd8cbffa56023986ef
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-14 21:12:16 -0700

    Added styling for single product page

---
+++ themes/inhabitent/single-product.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <header class="entry-header">
+        <?php if ( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'large' ); ?>
+        <?php endif; ?>
+      </header><!-- .entry-header -->
+
+      <div class="entry-content">
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <p class="entry-price"><?php echo CFS()->get( 'price' ); ?></p>
+        <?php the_content(); ?>
+        <?php
+          wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+            'after'  => '</div>',
+          ) );
+        ?>
+      </div><!-- .entry-content -->
+
+    </article><!-- #post-## -->
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
