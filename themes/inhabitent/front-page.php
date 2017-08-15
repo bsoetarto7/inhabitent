4c7e0e8c37792c2d475eb15a9f66b64172412d70
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-14 22:58:56 -0700

    Added front page hero image and style

---
+++ themes/inhabitent/front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+      <section class="home-hero">
+        <img src=<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-full.svg alt="hero image"/>
+      </section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
