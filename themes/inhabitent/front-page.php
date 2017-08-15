e3bf13974261c01d72b234fc9f361964145faa48
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-15 10:15:20 -0700

    Added journal post in the front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -12,7 +12,25 @@ get_header(); ?>
       <section class="home-hero">
         <img src=<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-full.svg alt="hero image"/>
       </section>
-
+			<section class="container-fluid home-blog-post">
+				<h2 class="text-uppercase text-center">inhabitent journal</h2>
+				<?php
+					$args = array( 'post_type' => 'post', 'orderby' => 'date', 'order' => 'DESC', 'numberposts' => '3' );
+					$journal_posts = get_posts( $args ); // returns an array of posts
+				?>
+				<div class="flex-container-no-wrap">
+					<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+						<div class="journal-post-item flex-item-33">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'large' ); ?>
+							<?php endif; ?>
+							<p><?php the_date(); ?> / <?php esc_html( comments_number( '0 Comments', '1 Comment', '% Comments' ) ); ?></p> 
+							<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+							<a href=<?php echo get_post_permalink() ?>>Read More</a>
+						</div>
+					<?php endforeach; wp_reset_postdata(); ?>
+				</div>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
