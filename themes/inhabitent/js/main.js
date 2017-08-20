c5ab6f97c332ef6fe234a7322346fc891baeac29
bsoetarto7 <bsoetarto7@gmail.com>
2017-08-20 12:29:52 -0700

    Refactored js & css

--- themes/inhabitent/js/main.js
+++ themes/inhabitent/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
   // Search icon animation
   $('#search-form-input .icon-search').click(function(){
-    $('#search-form-input .search-field').addClass('search-field-focus');
+    $('#search-form-input .search-field').toggleClass('search-field-focus');
   });
   $('#search-form-input .search-field').focusout(function(){
     if($('#search-form-input .search-field').hasClass('search-field-focus')){
